a5cd0cfdfca69fc972d9b6dd1ea2d1969776ae37
azagdanski <andrzej.zagdanski@aveliore.com>
2022-03-03 15:02:22 +0100

    Add linecounter.sh

---
+++ linecounter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "Select file [e.g. /home/joe/file.txt]: " INPUT
+if [ -e "$INPUT" ]; then
+    if [ -f "$INPUT" ]; then
+        LINECOUNT=$(wc -l < $INPUT)
+        echo "Number of lines: $LINECOUNT"
+    fi
+    if [ -d "$INPUT" ]; then
+        echo "$INPUT is a directory!"
+        exit 1
+    fi
+else 
+    echo "$INPUT does not exist"
+	exit 1
+fi
+exit
