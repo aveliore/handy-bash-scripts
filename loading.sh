f8e35241e0e1d0a37c54916f6c4317038a170540
azagdanski <andrzej.zagdanski@aveliore.com>
2022-06-13 18:52:05 +0200

    Add loading.sh

---
+++ loading.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Loading animation to be used in other scripts. 
+while true;
+do
+    # Frame #1
+    printf "\r< Loading..." 
+    sleep 0.5
+    # Frame #2 
+    printf "\r> Loading..." 
+    sleep 0.5 
+done
